e53d485092cf89ba5cf001bea12a653f5ba841a5
Peter_V <gordienkopeter@gmail.com>
2017-12-26 12:41:13 +0200

    added post controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function all()
+    {
+        dd(213232);
+    }
+}
